dbb0afc239d67e074189d4bab343114f1eb5772d
RafaRisco <rafaelrisconardiz@gmail.com>
2021-07-26 13:34:47 -0500

    some api test included

--- pokemons/tests.py
+++ pokemons/tests.py
@@ -10,12 +10,28 @@ class PokemonApiTestCase(TestCase):
     def setUp(self):
         self.new_pokemon = Pokemon.objects.create(
             name='Bulbasaur',
-            type_one=TypeOptions.choices[1][0],
-            type_two=TypeOptions.choices[2][0]
+            type_one=TypeOptions.DARK,
+            type_two=TypeOptions.DRAGON
         )
 
+
     def test_view(self):
-        url = reverse('pokemons:pokemon_list')
+        url = reverse('pokemons:all_pokemon_list')
         client = APIClient()
         response = client.get(url, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_pokemon_in_view(self):
+        url = reverse('pokemons:all_pokemon_list')
+        client = APIClient()
+        response = client.get(url, format='json')
+        response_type_one = response.data[0]['type_one']
+        response_type_two = response.data[0]['type_two']
+        self.assertEqual(self.new_pokemon.type_one, response_type_one)
+        self.assertEqual(self.new_pokemon.type_two, response_type_two)
+
+    def test_pokemon_not_in_view(self):
+        url = reverse('pokemons:filtered_pokemon_list', kwargs={'filter': 'Electric'})
+        client = APIClient()
+        response = client.get(url, format='json')
+        self.assertEqual(len(response.data), 0)
--- pokemons/urls.py
+++ pokemons/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import PokemonList
 
 urlpatterns = [
-    path('pokemon_list/', PokemonList.as_view(), name='pokemon_list'),
-    path('<str:filter>/pokemon_list/', PokemonList.as_view(), name='pokemon_list'),
+    path('pokemon_list/', PokemonList.as_view(), name='all_pokemon_list'),
+    path('<str:filter>/pokemon_list/', PokemonList.as_view(), name='filtered_pokemon_list'),
 ]
