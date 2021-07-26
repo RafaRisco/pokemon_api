63a8c692a5a6e7efc54ab197f1b82b23618cc977
RafaRisco <rafaelrisconardiz@gmail.com>
2021-07-26 06:17:00 -0500

    pokemon admin registered

--- pokemons/admin.py
+++ pokemons/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Pokemon
+
+class PokemonAdmin(admin.ModelAdmin):
+    list_display = [
+        'name',
+        'type_one',
+        'type_two',
+        'total',
+        'hp',
+        'attack',
+        'defense',
+        'sp_attack',
+        'sp_def',
+        'speed',
+        'generation',
+        'legendary'
+    ]
+
+admin.site.register(Pokemon, PokemonAdmin)
